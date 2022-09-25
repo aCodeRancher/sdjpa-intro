07e6bbafc9fcf3e69b13501734dff58728850db7
helen <hhma@alumni.cpp.edu>
2022-09-25 14:51:14 -0400

    add tests for assignment 4

--- src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -1,6 +1,10 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.domain.AuthorUuid;
+import guru.springframework.sdjpaintro.domain.BookUuid;
+import guru.springframework.sdjpaintro.repositories.AuthorUuidRepository;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
+import guru.springframework.sdjpaintro.repositories.BookUuidRepository;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Created by jt on 7/4/21.
@@ -23,6 +28,12 @@ public class MySQLIntegrationTest {
     @Autowired
     BookRepository bookRepository;
 
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+
+    @Autowired
+    AuthorUuidRepository authorUuidRepository;
+
     @Test
     void testMySQL() {
         long countBefore = bookRepository.count();
@@ -30,6 +41,24 @@ public class MySQLIntegrationTest {
 
     }
 
+    @Test
+    void testSaveBookUuid(){
+        BookUuid bookUuid = new BookUuid();
+        bookUuid.setTitle("Hibernate and Spring Data JPA");
+        bookUuid.setIsbn("45678");
+        bookUuid.setPublisher("Spring Framework Guru");
+        BookUuid bookSaved = bookUuidRepository.save(bookUuid);
+        assertTrue(bookUuidRepository.findById(bookSaved.getId())!=null);
+    }
+
+    @Test
+    void testSaveAuthorUuid(){
+        AuthorUuid authorUuid = new AuthorUuid();
+        authorUuid.setFirstName("John");
+        authorUuid.setLastName("Thompson");
+        AuthorUuid authorSaved = authorUuidRepository.save(authorUuid);
+        assertTrue(authorUuidRepository.findById(authorSaved.getId())!=null);
+    }
 }
 
 
