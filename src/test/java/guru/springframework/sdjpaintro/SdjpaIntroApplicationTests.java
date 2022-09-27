ba9ef904c37683d97811c80677ca28031243be3b
helen <hhma@alumni.cpp.edu>
2022-09-26 21:16:30 -0400

    add SdjpaIntroApplicationTests.java

--- src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,13 +1,31 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.repository.AuthorRepository;
+import guru.springframework.sdjpaintro.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class SdjpaIntroApplicationTests {
 
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    AuthorRepository authorRepository;
+
     @Test
-    void contextLoads() {
+    void testBookRepository() {
+        long countBooks = bookRepository.count();
+
+        assertThat(countBooks).isEqualTo(5);
+
+        long countAuthors = authorRepository.count();
+        assertThat(countAuthors).isEqualTo(3);
     }
 
 }
